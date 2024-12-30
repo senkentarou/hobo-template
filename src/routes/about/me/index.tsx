5bc5420ffade6f54772692895601ad9a4ce14c5b
senkentarou <masahiro-senda@c-fo.com>
2024-12-30 23:43:47 +0900

    文章校正

--- src/routes/about/me/index.tsx
+++ src/routes/about/me/index.tsx
@@ -1,23 +1,34 @@
-import { PageTitle, Paragraph } from '@freee_jp/vibes';
+import { BulletedList, PageTitle, Paragraph, SectionTitle } from '@freee_jp/vibes';
 import { createFileRoute } from '@tanstack/react-router';
 
 const RouteComponent = () => {
   return (
     <>
       <PageTitle>senkentarouの紹介</PageTitle>
+      <SectionTitle mt={0.5}>略歴</SectionTitle>
       <Paragraph>
         1995年生まれ。大阪府出身。
         <br />
         同志社大学・大学院を経て2020年にフリー株式会社に新卒入社。
         入社よりアプリケーションエンジニアとして主にfreee申告・freee販売・freee工数管理(旧freeeプロジェクト管理)サービスの開発・運用・保守を経験。
         <br />
-        現在はマネジメント領域に注力しており、20名規模の開発組織のEM(EngineerManager)として各メンバーの成長支援や組織の一体感醸成、さらにはその結果生み出される開発生産性の向上に取り組んでいる。
+        現在はマネジメント領域に注力しており、20名規模の開発組織のEM(EngineeringManager)として各メンバーの成長支援や組織の一体感醸成、さらにはその結果生み出される開発生産性の向上に取り組んでいる。
         <br />
         名前の由来は小学生のときのあだ名。なんか犬っぽいね、という話となぜか名字の一部を改変するのが組み合わさり、さらにはなぜか太郎を付けてフルネームのようにしたいという要望から誕生した記憶。
         漢字表記は千犬太郎。
         <br />
         理工学修士(情報工学)
       </Paragraph>
+      <SectionTitle mt={0.5}>関連リンク</SectionTitle>
+      <BulletedList
+        listContents={[
+          {
+            value: 'GitHub Account',
+            url: 'https://github.com/senkentarou',
+            target: '_blank',
+          },
+        ]}
+      />
     </>
   );
 };
