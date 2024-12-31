fe25d002f23605231835b7a59428e1907e359556
senkentarou <masahiro-senda@c-fo.com>
2024-12-31 16:55:47 +0900

    package整理

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths"; // tsconfig.paths.jsonの内容をviteに読み込むプラグイン
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths'; // tsconfig.paths.jsonの内容をviteに読み込むプラグイン
 
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(), TanStackRouterVite(), tsconfigPaths()],
 });
