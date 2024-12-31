2f8e93e6b7407d7b80aeb4b63fa582809ca11e8c
senkentarou <masahiro-senda@c-fo.com>
2024-12-31 17:28:49 +0900

    asset読み込みのため相対パス指定

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths'; // tsconfig.paths.jsonの内容をviteに読み込むプラグイン
 
 export default defineConfig({
+  base: './',
   build: {
     outDir: 'docs',
   },
